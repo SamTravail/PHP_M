879e440d8c53ef5a190dd93cd3eb3f9032064bba
Samuel <slanguet@protonmail.com>
2022-06-30 16:28:12 +0200

    exercice plus grand

--- index.php
+++ index.php
@@ -1,23 +1,10 @@
 <?php
 include('includes/header.php');
 include('includes/fonctions.php');
-
 ?>
+
 <main>
     <?php
-
-    // $tabnbs = [27, 15, 34, 379, 248, 5643, 81, 211, 999, 142, 300, 572];
-
-    // $nombres = "<ul>";
-    // foreach ($tabnbs as $tabnb) {
-    //     $nombres .= "<li>$pairImpair</li>";
-    //     if ($tabnb % 2 === 0) {
-    //         echo "<li>$tabnb est pair</li>";
-    //     } else {
-    //         echo "<li>$tabnb est impair</li>";
-    //     }
-    // }
-    // $nombres .= "</ul>";
 
     $users = [
         "firstname" => 'Mike',
@@ -37,7 +24,6 @@ include('includes/fonctions.php');
     }
     
     ?>
-
 </main>
 
 <?php
