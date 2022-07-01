1784e82853072e78a0fc66eaca98f0c7388e71b9
Samuel <slanguet@protonmail.com>
2022-07-01 12:26:26 +0200

    exercice creation d une carte

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
+
+use LDAP\Result;
+
 include('includes/header.php');
 include('includes/fonctions.php');
 ?>
@@ -64,18 +67,45 @@ $json_peoples = '[{
     "image" : "https://ximg.es/64x64/000/fff"
 }]';
 
-debug($json_peoples);
+// debug($json_peoples);
 
-$peoples_array_php = json_decode($json_peoples,true);
+// $peoples_array_php = json_decode($json_peoples,true);
 
-debug($peoples_array_php);
+// debug($peoples_array_php);
+
+$users = file_get_contents("./users.json");
+debug($users);
+
+$users_array_php = json_decode($users,true);
 
 
-    ?>
+echo(count($users_array_php['results']));
+
+$id_user = $users_array_php['results'];
+?>
+
+<div class="card" style="text-align: center;">
+    <img src="<?php echo $users_array_php['results'][0]['picture']['thumbnail']; ?>" alt="thumbnail_user"></img>
+    <h2><?php echo$users_array_php['results'][0]['name']['first']; ?>
+    <span> <?php echo$users_array_php['results'][0]['name']['last']; ?></span></h2>
+    <h3><?php echo$users_array_php['results'][0]['email']?></h3>
+    <h4><?php echo$users_array_php['results'][0]['dob']['age']; ?> ans</h4>
+    <p>adresse : <?php echo$users_array_php['results'][0]['location']['state']; ?></p>
+    <p>telephone : <?php echo$users_array_php['results'][0]['cell']; ?></p>
+</div>
+
+<?php 
+debug($users_array_php);
+
+
+
+?>
+<!-- 
     <img src="<?php 
-        echo $peoples_array_php[1]['image'];
+        // echo $peoples_array_php[1]['image'];
     
-    ?>"></img>
+    ?>" alt="Amy image"></img> -->
+
 </main>
 
 <?php
