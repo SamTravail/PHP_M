90b31880d2fc7938ddd9ce581d9b0f991fd0f1cf
Samuel <slanguet@protonmail.com>
2022-07-01 09:27:54 +0200

    exercice plus grand

--- index.php
+++ index.php
@@ -6,23 +6,37 @@ include('includes/fonctions.php');
 <main>
     <?php
 
-    $users = [
-        "firstname" => 'Mike',
-        "lastname" => 'Olganh',
-        "pseudo" => '',
-        "age" => '34',
-        "food" => '',
-        "sport" => '',
+    // $users = [
+    //     "firstname" => 'Mike',
+    //     "lastname" => 'Olganh',
+    //     "pseudo" => '',
+    //     "age" => '34',
+    //     "food" => '',
+    //     "sport" => '',
     
-    ];
-    foreach($users as $key => $value) {
-        if ($value == ""){
-            echo "$key : Aucune information<br>";
+    // ];
+    // foreach($users as $key => $value) {
+    //     if ($value == ""){
+    //         echo "$key : Aucune information<br>";
+    //     } else {
+    //         echo "$key : $value<br>";
+    //     }
+    // }
+    
+$nombres = [4,15,2,145,42,5,78,12];
+define("nb", 0);
+// trouver le nombre mai du tableau
+foreach($nombres as $nombre) {
+        if ($nombre > nb){
+            (nb == $nombre);
         } else {
-            echo "$key : $value<br>";
+            echo(nb);
         }
     }
-    
+
+
+
+
     ?>
 </main>
 
